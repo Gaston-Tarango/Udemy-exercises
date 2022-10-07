27dcb96a587e51b27cef88053ba6f64a5758f04a
Gaston Tarango <gaston.tarango@gmail.com>
2022-10-07 11:50:15 -0500

    udemy start

---
+++ pasta exercise.py
@@ -0,0 +1,18 @@
+import time
+
+name = input("What is your name? ")
+print("Hello " + name + " heres how to cook pasta")
+time.sleep(1)
+print("Boil some water")
+time.sleep(5)
+print("Wait for 5 minutes")
+time.sleep(2)
+print("Add Salt")
+time.sleep(2)
+print("Add Pasta")
+time.sleep(2)
+print("Cook and wait for 10 minutes")
+time.sleep(2)
+print("Remove water")
+time.sleep(2)
+print("Done!");
